1805cad0a7975a9f81f75457a9471ccf709f4026
tanjonaaa <hei.tanjona@gmail.com>
2023-07-13 10:18:54 +0300

    feat: created the form of the TodoCRUD class

--- App/Database/TodoCRUD.java
+++ App/Database/TodoCRUD.java
@@ -1,4 +1,9 @@
 package App.Database;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 * A static class that will be used to execute all the queries that refers to the app
 *       -Create a Todo
@@ -8,4 +13,25 @@ package App.Database;
 * Needs a Database connection
 */
 public class TodoCRUD {
+    private final Connection connection = ConnectionDB.getConnectionInstance();
+
+    public static List<Todo> allTodos(){
+        return new ArrayList<Todo>();
+    }
+
+    public static Todo getTodo(int id){
+
+    }
+
+    public static int createTodo(/*TODO*/){
+        return 0;
+    }
+
+    public static int updateTodo(/*TODO.id, TODO.infos*/){
+        return 0;
+    }
+
+    public static int deleteTodo(int id){
+        return 0;
+    }
 }
