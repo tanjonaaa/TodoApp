5931bb08bbdd1bfc2570a3ae7625538b7977abef
tanjonaaa <hei.tanjona@gmail.com>
2023-07-13 09:39:33 +0300

    feat: added fake db connection infos

--- App/Database/Credentials.java
+++ App/Database/Credentials.java
@@ -1,4 +1,8 @@
 package App.Database;
 
 public class Credentials {
+    public static final String DB_HOST = "localhost:5432";
+    public static final String DB_NAME = "TodoApp";
+    public static final String DB_USER = "test";
+    public static final String DB_PASS = "test";
 }
